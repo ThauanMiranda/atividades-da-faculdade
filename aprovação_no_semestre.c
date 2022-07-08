825f9801eeb6e2a84eb2f378e45b1b27c36c72bf
Thauan Miranda <thauan10saopaulo@gmail.com>
2022-07-08 00:01:39 -0300

    programa de aprovação
    programa em desenvolvimento que informa a aprovação do aluno com base em sua informações

---
+++ aprovação_no_semestre.c
@@ -0,0 +1,29 @@
+#include<stdio.h> // programa em desenvolvimento 
+int main(){
+
+    int frequencia;
+    float nota1,nota2,avd,media;
+
+    printf("informe a nota da sua primeira prova:");
+    scanf("%f",&nota1);
+
+    printf("informe a nota da sua segunda prova:");
+    scanf("%f",&nota2);
+
+    printf("informe a nota da sua avd:");
+    scanf("%f",&avd);
+
+    printf("informe seu porcentual de frequencia:");
+    scanf("%d",&frequencia);
+
+    media = (nota1 + nota2 + avd) /3;
+
+    if(media >= 6 && frequencia>=70);
+    printf("aprovado");
+    //else //erro aqui
+
+    //if(media < 6 && frequencia <70);
+    printf("reprovado");
+
+    return 0;
+}
